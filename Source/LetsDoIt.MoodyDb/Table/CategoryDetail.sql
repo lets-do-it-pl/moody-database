04252217fb3f6214087a7715e5f6a3e85d89b201
Saban Durna <sabandurna@gmail.com>
2020-11-13 05:55:08 +0100

    Create Database Solution Add Category, CategoryDetail, Client, VersionHistory tables

---
+++ Source/LetsDoIt.MoodyDb/Table/CategoryDetail.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CategoryDetail]
+(
+	[Id]           INT             NOT NULL     PRIMARY KEY,
+	[CategoryId]   INT             NOT NULL,
+    [Order]        INT             NOT NULL,
+    [Image]        VARBINARY (MAX) NOT NULL,
+    [CreateDate]   DATETIME2 (7)   NOT NULL,
+    [ModifiedDate] DATETIME2 (7)   NULL,
+    [IsDeleted]    BIT             NOT NULL,
+    CONSTRAINT [FK_CategoryDetails_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+)
---
+++ Source/LetsDoIt.MoodyDb/Table/VersionHistory.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[VersionHistory]
+(
+	[Id]            BIGINT         NOT NULL     PRIMARY KEY,	
+    [VersionNumber] NVARCHAR (100) NOT NULL,
+    [CreateDate]    DATETIME2 (7)  NOT NULL
+)
