2372e212bb4cbc34ac8a3a1098e939746589dcc2
Saban Durna <sabandurna@gmail.com>
2020-11-16 23:07:57 +0100

    Add User Table

---
+++ Source/LetsDoIt.MoodyDb/Table/User.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id]			INT				NOT NULL	PRIMARY KEY,
+	[Username]		VARCHAR(500)	NOT NULL,
+	[Password]		VARCHAR(1000)	NOT NULL,
+	[NameSurname]	VARCHAR(500)	NOT NULL,
+	[Email]			VARCHAR(500)	NOT NULL,
+	[IsActive]		BIT				NOT NULL	DEFAULT		0,
+	[UserType]		CHAR(1)			NOT NULL	DEFAULT		'S',
+	[CreatedDate]	DATETIME		NOT NULL	DEFAULT		GETDATE(),
+	[CreatedBy]		INT				NOT NULL,
+	[ModifiedDate]	DATETIME		NULL,
+	[ModifiedBy]	INT				NULL,
+	[IsDeleted]		BIT				NOT NULL	DEFAULT		0
+)
