7574ef92dc1d20882dc775ec013796c65273fb35
Saban Durna <sabandurna@gmail.com>
2020-11-17 23:00:59 +0100

    Make Order Column Unique Replace nvarchar with varchar in Version History

--- Source/LetsDoIt.MoodyDb/Table/CategoryDetail.sql
+++ Source/LetsDoIt.MoodyDb/Table/CategoryDetail.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[CategoryDetail]
 (
-	[Id]			INT             NOT NULL     PRIMARY KEY IDENTITY(1,1),
+	[Id]			INT             NOT NULL    PRIMARY KEY IDENTITY(1,1),
 	[CategoryId]	INT             NOT NULL,
-    [Order]			INT             NOT NULL,
-    [Image]			VARBINARY (MAX) NOT NULL,
+	[Order]			DECIMAL(18,5)   NOT NULL	UNIQUE,
+	[Image]			VARBINARY (MAX) NOT NULL,
 	[CreatedDate]	DATETIME		NOT NULL	DEFAULT GETDATE(),
 	[CreatedBy]		INT				NOT NULL,
 	[ModifiedDate]	DATETIME		NULL,
 	[ModifiedBy]	INT				NULL,
 	[IsDeleted]		BIT				NOT NULL	DEFAULT 0
-    CONSTRAINT [FK_CategoryDetails_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+	CONSTRAINT [FK_CategoryDetails_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
 )
--- Source/LetsDoIt.MoodyDb/Table/VersionHistory.sql
+++ Source/LetsDoIt.MoodyDb/Table/VersionHistory.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[VersionHistory]
 (
 	[Id]                BIGINT         NOT NULL     PRIMARY KEY IDENTITY(1,1),	
-    [VersionNumber]     NVARCHAR (100) NOT NULL,
-    [CreatedDate]       DATETIME2 (7)  NOT NULL
+	[VersionNumber]     VARCHAR (100)  NOT NULL,
+	[CreatedDate]       DATETIME2 (7)  NOT NULL
 )
