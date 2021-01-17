a9f35f57c49fa3eaac67800e3c1f9c02f4cdbb0b
M7ZAHIT <47788535+M7ZAHIT@users.noreply.github.com>
2021-01-17 14:28:46 +0100

    MOODY-28-Creating-Store-Procedure-For-AutoComplete-Search Created Stored Procedure for AutoCompleteSearch

---
+++ Source/LetsDoIt.MoodyDb/Stored Procedure/AutoCompleteSearch.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[AutoCompleteSearch]
+	@EnteredValue nvarchar(500)
+AS
+BEGIN
+	SELECT UserName
+	FROM Client
+	WHERE Client.UserName Like  @EnteredValue + '%' 
+
+	UNION  ALL
+
+	SELECT Name
+	FROM Category	
+	WHERE Category.Name Like  @EnteredValue + '%' 
+
+	UNION ALL
+
+	SELECT Username
+	FROM [dbo].[User]
+	WHERE [dbo].[User].Username Like  @EnteredValue + '%' 
+
+	END
+RETURN 0
