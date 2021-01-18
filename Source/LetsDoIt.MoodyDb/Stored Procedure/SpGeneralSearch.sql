0f5ffcbee1d5f291312a192f47726b8424024519
M7ZAHIT <47788535+M7ZAHIT@users.noreply.github.com>
2021-01-18 22:57:31 +0100

    MOODY-28-Creating-Store-Procedure-For-AutoComplete-Search Added Case Insenstivity

--- Source/LetsDoIt.MoodyDb/Stored Procedure/SpGeneralSearch.sql
+++ Source/LetsDoIt.MoodyDb/Stored Procedure/SpGeneralSearch.sql
@@ -4,12 +4,10 @@ AS
 
 	SELECT 'Category' AS [Type], Name
 	FROM Category	
-	WHERE IsDeleted = 0 AND Category.Name Like  @searchKey + '%' 
+	WHERE IsDeleted = 0 AND  Category.Name Like  @searchKey + '%' COLLATE SQL_Latin1_General_CP1_CI_AS
 
 	UNION ALL
 
 	SELECT 'User' AS [Type], Username AS Name
 	FROM [dbo].[User]
-	WHERE IsDeleted = 0 AND [dbo].[User].Username Like  @searchKey + '%' 
-
-
+	WHERE IsDeleted = 0 AND [dbo].[User].Username Like  @searchKey + '%' COLLATE SQL_Latin1_General_CP1_CI_AS
